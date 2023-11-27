22c9191f374f41f1f762781d147c0ea18129e364
rlharsh <ronald.harsh@protonmail.com>
2023-11-27 17:32:20 -0600

    Update Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,18 +8,15 @@ const Home = () => {
     <div className="main-container">
         <section className='introduction'>
             <div className='introduction__top'>
-                <div className="green-blob">d</div>
-                <div className="blue-blob"></div>
                 <p>Hello there. I am</p>
                 <h1>Ronald Harsh</h1>
-                <p><span className="green">> Front-end developer</span></p>
+                <p><span className="green">>Front-end developer</span></p>
             </div>
 
-            <CodeBlock
+           <CodeBlock
   language="javascript"
   value={
 `// find my profile on Github:
-
 const githubLink = "https://github.com/rlharsh";`
   }
 />
